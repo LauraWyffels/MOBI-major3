8ae49c12e23c70b26c1e44839443e78478ce603b
LauraWyffels <laura.wyffels@student.howest.be>
2018-01-28 21:32:57 +0100

    detail responsive behalve content en practical

--- src/view/events/detail.php
+++ src/view/events/detail.php
@@ -28,86 +28,82 @@
     <ul  class="tags"><?php
     foreach($event['tags'] as $tag): ?><li class="tag"><?php echo $tag['tag'];?></li><?php endforeach;?></ul>
   </section>
-  <section>
+
+  <div class="DetailSection">
+    <section>
       <img src="assets/images/events/<?php echo $event['code'];?>.jpg" alt="" width="500" height="360" class="eventImage">
-  </section>
-  <section class="GeelInfoBlok">
-      <!-- <article class="adres infoblok"> -->
-        <h2 class="hide">Adres</h2>
-        <img src="assets/images/icon_map.svg" alt="Adress" width="34" height="60" class="iconAdres">
-
-        <?php if($event['address'] === "Diverse locaties"){ ?>
-          <p><?php echo $event['location'];?></p>
-        <?php
-        }else if ($event['address'] === $event['location']) {
-        ?>
-        <p class="address"><?php echo $event['address']; ?>
-        <br><?php echo $event['postal'];?> <?php echo $event['city'];?>
-        <?php  }else { ?>
-
-        <p class="address"><?php echo $event['address']; ?>
-          <br><?php echo $event['postal'];?> <?php echo $event['city'];?>
-        </p>
-        <p class="locatie"><br><?php echo $event['location'];?></p>
-
-
-        <?php
-          }
-        ?>
-        <svg  width="4" height="300" viewBox="0 0 4 300"  class="yellowline">
-          <g>
-            <line x1="2.1" y1="0" x2="2.1" y2="300"/>
-          </g>
-        </svg>
-      <!-- </article> -->
-
-      <!-- <article class="articleDate infoblok"> -->
-        <h2 class="hide">Datum</h2>
-        <img src="assets/images/icon_time.svg" alt="Datum" width="48,82" height="60" class="iconDate">
-        <?php if (date('d', strtotime($event['start'])) === date('d', strtotime($event['end']))){?>
-          <p class="datumDetail"><?php echo date('d', strtotime($event['start']));?> september 2018</p>
-        <?php
-          }else{
-        ?>
-          <p class="datumDetail">van <?php echo date('d', strtotime($event['start']));?> september 2018
-          <br>tot <?php echo date('d', strtotime($event['end']));?> september 2018</p>
-        <?php
-          }
-        ?>
-        <br>
-        <p class="timeDetail">start: <?php echo date('G', strtotime($event['start']));?>:<?php echo date('i', strtotime($event['start']));?> uur
-          <br>end: <?php echo date('G', strtotime($event['end']));?>:<?php echo date('i', strtotime($event['end']));?> uur
-        </p>
-
-        <svg  width="4" height="300" viewBox="0 0 4 300" class="yellowline">
-        <g>
-          <line x1="2.1" y1="0" x2="2.1" y2="300"/>
-        </g>
-        </svg>
-      <!-- </article> -->
-
-      <!-- <article class="organisatoren infoblok"> -->
-        <h2 class="hide">Organisatoren</h2>
-        <img src="assets/images/icon_organisator.svg" alt="Organisatoren" width="84,67" height="60" class="iconOrganisator">
-        <p><ul  class="organisators"><?php foreach($event['organisers'] as $name): ?><li> - <?php echo $name['name'];?></li><?php endforeach;?></ul></p>
-      <!-- </article> -->
     </section>
+    <section class="GeelInfoBlok">
+      <h2 class="hide">Adres</h2>
+      <img src="assets/images/icon_map.svg" alt="Adress" width="34" height="60" class="iconAdres">
 
-    <section class="content">
-      <!-- <h2 class=""><?php echo $event['title']; ?></h2> -->
-      <h3 class="practicalTitle">Informatie</h3>
-      <p class="infoP"><?php echo $event['content'];?></p>
-    </section>
-
-    <section class="practical">
-      <?php if($event['practical'] === ""){ ?>
-      <?php echo " ";
-        }else{
+      <?php if($event['address'] === "Diverse locaties"){ ?>
+        <p><?php echo $event['location'];?></p>
+      <?php
+      }else if ($event['address'] === $event['location']) {
       ?>
-      <h3 class="practicalTitle">Praktisch</h3>
+      <p class="address"><?php echo $event['address']; ?>
+      <br><?php echo $event['postal'];?> <?php echo $event['city'];?>
+      <?php  }else { ?>
+
+      <p class="address"><?php echo $event['address']; ?>
+        <br><?php echo $event['postal'];?> <?php echo $event['city'];?>
+      </p>
+      <p class="locatie"><?php echo $event['location'];?></p>
       <?php
         }
       ?>
-      <p class="infoP"><?php echo $event['practical'];?></p>
+
+      <!-- <svg  width="4" height="300" viewBox="0 0 4 300"  class="yellowline">
+        <g>
+          <line x1="2.1" y1="0" x2="2.1" y2="300"/>
+        </g>
+      </svg> -->
+
+      <h2 class="hide">Datum</h2>
+      <img src="assets/images/icon_time.svg" alt="Datum" width="48,82" height="60" class="iconDate">
+      <?php if (date('d', strtotime($event['start'])) === date('d', strtotime($event['end']))){?>
+        <p class="datumDetail"><?php echo date('d', strtotime($event['start']));?> september 2018</p>
+      <?php
+        }else{
+      ?>
+        <p class="datumDetail">van <?php echo date('d', strtotime($event['start']));?> september 2018
+        <br>tot <?php echo date('d', strtotime($event['end']));?> september 2018</p>
+      <?php
+        }
+      ?>
+
+      <p class="timeDetail">start: <?php echo date('G', strtotime($event['start']));?>:<?php echo date('i', strtotime($event['start']));?> uur
+        <br>end: <?php echo date('G', strtotime($event['end']));?>:<?php echo date('i', strtotime($event['end']));?> uur
+      </p>
+
+      <!-- <svg  width="4" height="300" viewBox="0 0 4 300" class="yellowline">
+      <g>
+        <line x1="2.1" y1="0" x2="2.1" y2="300"/>
+      </g>
+      </svg> -->
+
+      <h2 class="hide">Organisatoren</h2>
+      <img src="assets/images/icon_organisator.svg" alt="Organisatoren" width="84,67" height="60" class="iconOrganisator">
+      <p><ul  class="organisators"><?php foreach($event['organisers'] as $name): ?><li> - <?php echo $name['name'];?></li><?php endforeach;?></ul></p>
     </section>
+  </div>
+
+  <section class="content">
+    <!-- <h2 class=""><?php echo $event['title']; ?></h2> -->
+    <h3 class="practicalTitle">Informatie</h3>
+    <p class="infoP"><?php echo $event['content'];?></p>
+  </section>
+
+  <section class="practical">
+    <?php if($event['practical'] === ""){ ?>
+    <?php echo " ";
+      }else{
+    ?>
+    <h3 class="practicalTitle">Praktisch</h3>
+    <?php
+      }
+    ?>
+    <p class="infoP"><?php echo $event['practical'];?></p>
+  </section>
 </main>
